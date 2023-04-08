e2ab7a8c403f6a531323b91dc39403b081237429
digitalman <digitalman@a1.net>
2023-04-09 00:24:25 +0200

    Update .env

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'Flutter Demo',
+        title: 'Stamping Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
